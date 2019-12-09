ce4b455ec300b78fdcb82931ee94dd8c2d5451de
Penjahat <58440372+BL4CKID@users.noreply.github.com>
2019-12-10 04:50:49 +0700

    Update www.py

--- userbot/modules/www.py
+++ userbot/modules/www.py
@@ -62,7 +62,7 @@ async def neardc(event):
                      f"This Datacenter : `{result.this_dc}`")
 
 
-@register(outgoing=True, pattern="^.ping$")
+@register(outgoing=True, pattern="^.p$")
 async def pingme(pong):
     """ For .ping command, ping the userbot from any chat.  """
     start = datetime.now()
@@ -72,12 +72,4 @@ async def pingme(pong):
     await pong.edit("`Pong!\n%sms`" % (duration))
 
 
-CMD_HELP.update(
-    {"speed": ".speed\
-    \nUsage: Does a speedtest and shows the results."})
-CMD_HELP.update(
-    {"dc": ".dc\
-    \nUsage: Finds the nearest datacenter from your server."})
-CMD_HELP.update(
-    {"ping": ".ping\
-    \nUsage: Shows how long it takes to ping your bot."})
+
