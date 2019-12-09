27c3f5885ea5b773c4bcb0a73fcab7936c2f4d37
Penjahat <58440372+BL4CKID@users.noreply.github.com>
2019-12-10 03:46:23 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -136,6 +136,8 @@ CMD_HELP.update({
 \n.paste <text/reply> Create a paste or a shortened url using dogbin (https://del.dog/)\
 \n.getpaste Gets the content of a paste or shortened url from dogbin (https://del.dog/)\    
 \n.rbg Removes the background of images."\
+\n.car <text> [or reply] Beautify your code using carbon.now.sh\
+\n.crblang <text> to set language for your code."\
 })
 
 CMD_HELP.update({
