3e49d983f7361d2ee19fc8541da5b6a43542ded2
Penjahat <58440372+BL4CKID@users.noreply.github.com>
2019-12-10 03:32:08 +0700

    Update purge.py

--- userbot/modules/purge.py
+++ userbot/modules/purge.py
@@ -13,9 +13,9 @@ from userbot import BOTLOG, BOTLOG_CHATID, CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern="^.purge$")
+@register(outgoing=True, pattern="^.pu$")
 async def fastpurger(purg):
-    """ For .purge command, purge all messages starting from the reply. """
+    """ For .pu command, purge all messages starting from the reply. """
     chat = await purg.get_input_chat()
     msgs = []
     itermsg = purg.client.iter_messages(chat, min_id=purg.reply_to_msg_id)
@@ -47,9 +47,9 @@ async def fastpurger(purg):
     await done.delete()
 
 
-@register(outgoing=True, pattern="^.purgeme")
+@register(outgoing=True, pattern="^.pm")
 async def purgeme(delme):
-    """ For .purgeme, delete x count of your latest message."""
+    """ For .pm, delete x count of your latest message."""
     message = delme.text
     count = int(message[9:])
     i = 1
@@ -74,9 +74,9 @@ async def purgeme(delme):
     await smsg.delete()
 
 
-@register(outgoing=True, pattern="^.del$")
+@register(outgoing=True, pattern="^.d$")
 async def delete_it(delme):
-    """ For .del command, delete the replied message. """
+    """ For .d command, delete the replied message. """
     msg_src = await delme.get_reply_message()
     if delme.reply_to_msg_id:
         try:
@@ -89,25 +89,6 @@ async def delete_it(delme):
             if BOTLOG:
                 await delme.client.send_message(
                     BOTLOG_CHATID, "Well, I can't delete a message")
-
-
-@register(outgoing=True, pattern="^.edit")
-async def editer(edit):
-    """ For .editme command, edit your last message. """
-    message = edit.text
-    chat = await edit.get_input_chat()
-    self_id = await edit.client.get_peer_id('me')
-    string = str(message[6:])
-    i = 1
-    async for message in edit.client.iter_messages(chat, self_id):
-        if i == 2:
-            await message.edit(string)
-            await edit.delete()
-            break
-        i = i + 1
-    if BOTLOG:
-        await edit.client.send_message(BOTLOG_CHATID,
-                                       "Edit query was executed successfully")
 
 
 @register(outgoing=True, pattern="^.sd")
@@ -126,29 +107,13 @@ async def selfdestruct(destroy):
 
 
 CMD_HELP.update({
-    'purge':
-    '.purge\
-        \nUsage: Purges all messages starting from the reply.'
-})
-
-CMD_HELP.update({
-    'purgeme':
-    '.purgeme <x>\
-        \nUsage: Deletes x amount of your latest messages.'
-})
-
-CMD_HELP.update({"del": ".del\
-\nUsage: Deletes the message you replied to."})
-
-CMD_HELP.update({
-    'edit':
-    ".edit <newmessage>\
-\nUsage: Replace your last message with <newmessage>."
-})
-
-CMD_HELP.update({
-    'sd':
-    '.sd <x> <message>\
-\nUsage: Creates a message that selfdestructs in x seconds.\
-\nKeep the seconds under 100 since it puts your bot to sleep.'
+    "chat":
+    ".pu Purges all messages starting from the reply.\
+\n.pm Deletes x amount of your latest messages.\
+\n.d Deletes the message you replied to.\
+\n.sd Creates a message that selfdestructs in x seconds.\
+\n.filters Lists all active userbot filters in a chat.\
+\n.filter Add Reply to a message with .filter <keyword>.\
+\n.stop Stops the specified filter.\
+\n.rmbotfilters.\"
 })
